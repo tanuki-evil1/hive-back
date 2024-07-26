53b2eefdb8f4f37ceb985f7f700ad22cae8de066
Tanuki Evil <tanuki.evil@yandex.ru>
2024-07-26 18:23:00 +0300

    del forms and templates

--- hive/users/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import User
-
-
-class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
